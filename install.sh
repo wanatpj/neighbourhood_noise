8f2d7573ce23a8611d62b52c59bfaa8490e0bd70
Paweł Wanat <pawel@wayve.ai>
2021-09-19 17:08:20 +0100

    add python distutil

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt update && sudo apt install bazel-bootstrap
 sudo apt update && sudo apt full-upgrade
+sudo apt install python3-dev
 
 sudo cp record_noise.service /etc/systemd/system/
 sudo systemctl daemon-reload
